63b0997e517a698027f4440cc7f8dbe6a176318e
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2021-06-23 15:36:07 -0300

    update assets/create categories/config svg.d.ts

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { View } from "react-native";
 import { styles } from "./styles";
 import { Profile } from "../../components/Profile";
 import { ButtonAdd } from "../../components/ButtonAdd";
+import { CategorySelect } from "../../components/CategorySelect";
 
 export function Home() {
   return (
@@ -10,6 +11,9 @@ export function Home() {
       <View style={styles.header}>
         <Profile />
         <ButtonAdd />
+      </View>
+      <View>
+        <CategorySelect />
       </View>
     </View>
   );
