2df22e991c3a5771e0712a5ebbe9bfff283fc40f
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2021-06-24 18:00:37 -0300

    finish screen appointmentecreate

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -3,26 +3,96 @@ import { styles } from "./styles";
 import { theme } from "../../global/styles/theme";
 import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Platform,
+  KeyboardAvoidingView,
+} from "react-native";
 import { CategorySelect } from "../../components/CategorySelect";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import { Feather } from "@expo/vector-icons";
+import { GuildIcon } from "../../components/GuildIcon";
+import { SmallInput } from "../../components/SmallInput";
+import { TextArea } from "../../components/TextArea";
+import { Button } from "../../components/Button";
 
 export function AppointmentCreate() {
   const [category, setCategory] = useState("");
 
   return (
-    <Background>
-      <Header title="Agendar partida" />
-      <Text style={styles.label}>Categoria</Text>
-      <CategorySelect
-        hasCheckBox
-        setCategory={setCategory}
-        categorySelect={category}
-      />
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <ScrollView>
+        <Background>
+          <Header title="Agendar partida" />
+          <Text
+            style={[
+              styles.label,
+              { marginLeft: 24, marginTop: 36, marginBottom: 18 },
+            ]}
+          >
+            Categoria
+          </Text>
+          <CategorySelect
+            hasCheckBox
+            setCategory={setCategory}
+            categorySelect={category}
+          />
 
-      <View style={styles.form}>
+          <View style={styles.form}>
+            <RectButton>
+              <View style={styles.select}>
+                {
+                  // <View style={styles.image} /> :
+                  <GuildIcon />
+                }
+                <View style={styles.selectBody}>
+                  <Text style={styles.label}>Selecione um servidor</Text>
+                </View>
 
-        
-      </View>
-    </Background>
+                <Feather name="chevron-right" color={theme.colors.heading} />
+              </View>
+            </RectButton>
+
+            <View style={styles.field}>
+              <View>
+                <Text style={styles.label}>Dia e mês</Text>
+                <View style={styles.column}>
+                  <SmallInput maxLength={2} />
+                  <Text style={styles.divider}>/</Text>
+                  <SmallInput maxLength={2} />
+                </View>
+              </View>
+              <View>
+                <Text style={styles.label}>Hora e minuto</Text>
+                <View style={styles.column}>
+                  <SmallInput maxLength={2} />
+                  <Text style={styles.divider}>/</Text>
+                  <SmallInput maxLength={2} />
+                </View>
+              </View>
+            </View>
+
+            <View style={[styles.field, { marginBottom: 12 }]}>
+              <Text style={styles.label}>Descrição</Text>
+              <Text style={styles.caracteresLimit}>Max 100 caracteres</Text>
+            </View>
+            <TextArea
+              multiline
+              maxLength={100}
+              numberOfLines={5}
+              autoCorrect={false}
+            />
+            <View style={styles.footer}>
+              <Button title="Agendar" />
+            </View>
+          </View>
+        </Background>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
